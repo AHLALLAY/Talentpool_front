4c1bdd5f1db1e6b5166a0c3183511643ea1ec602
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-14 15:24:20 +0100

    feat: Ajouter une bare latéral dans dashboard de recruteur

--- src/views/operator/dashboard.jsx
+++ src/views/operator/dashboard.jsx
@@ -1,13 +1,20 @@
 import NavBarOpearator from "../../components/operator/NavBarOpearator";
-import PostCard from "./PostCard";
+import PostCard from "../../components/operator/PostCard";
 
-export default function dashboard(){
-
+export default function dashboard() {
 
     return (
-        <div className="pt-20">
+        <div className="pt-20 flex h-screen">
             <NavBarOpearator />
-            <main className="p-6">
+            <aside className="w-64 bg-gray-100 p-4 border-r flex flex-col justify-start h-full">
+                <button className="mb-3 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">
+                    Ajouter Annonce
+                </button>
+                <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition">
+                    Voir les statistiques
+                </button>
+            </aside>
+            <main className="flex-1 p-6 bg-white">
                 <PostCard />
             </main>
         </div>
